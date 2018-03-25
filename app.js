9dc0c5934dfdb81b3c74bd5db5c4cd8b9255c955
juanchitox91 <37164311+juanchitox91@users.noreply.github.com>
2018-03-24 21:50:05 -0300

    commit curso leccion 13

---
+++ app.js
@@ -0,0 +1,22 @@
+'user-strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//cargar rutas
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//configurar cabeceras http
+
+//rutas base
+
+app.get('/pruebas', function(req, res){
+	res.status(200).send({message: 'Bienvenido al curso madafaca'});
+});
+
+
+module.exports = app;
